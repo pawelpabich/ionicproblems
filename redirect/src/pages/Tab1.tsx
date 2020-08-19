3bce399bbc1efa8146e37041416d5c58080fa514
Pawel Pabich <pawel@pabich.eu>
2020-08-19 17:08:51 +1000

    Broken redirect

--- redirect/src/pages/Tab1.tsx
+++ redirect/src/pages/Tab1.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import './Tab1.css';
+import {Redirect} from "react-router-dom";
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+interface Props {
+}
+
+interface State {
+    redirectTo: string | null;
+}
+
+class Tab1 extends React.Component<Props, State> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            redirectTo: null
+        };
+    }
+
+    render() {
+
+        if (this.state.redirectTo) {
+            return <Redirect to={this.state.redirectTo} />;
+        }
+
+        return <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 1</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonButton type="button" onClick={this.go}>Go To Tab2</IonButton>
+            </IonContent>
+        </IonPage>
+    }
+
+    go = async () => {
+        this.setState({redirectTo: "/tab2"})
+    };
+}
 
 export default Tab1;
+
--- redirect/src/pages/Tab2.tsx
+++ redirect/src/pages/Tab2.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import './Tab1.css';
+import {Redirect} from "react-router-dom";
 
-const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+interface Props {
+}
+
+interface State {
+    redirectTo: string | null;
+}
+
+class Tab2 extends React.Component<Props, State> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            redirectTo: null
+        };
+    }
+
+    render() {
+
+        if (this.state.redirectTo) {
+            return <Redirect to={this.state.redirectTo} />;
+        }
+
+        return <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 1</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonButton type="button" onClick={this.go}>Go To Tab1</IonButton>
+            </IonContent>
+        </IonPage>
+    }
+
+    go = async () => {
+        this.setState({redirectTo: "/tab1"})
+    };
+}
 
 export default Tab2;
+
+
